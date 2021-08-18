f18d6608b7132cb88c255fd09ee69f115407b98b
koray-eren <koray.eren@outlook.com>
2021-08-18 15:21:03 +1000

    added readme, refactored main file

--- main.rb
+++ main.rb
@@ -10,57 +10,41 @@ def printHeaders
 end
 
 def printAssetData(assets)
-    for i in assets do
-        puts "TEST"
+    for asset in assets do
+        printf("%-8s" % [asset[0] ]  )
+        
+        for j in (0..YEARS) do
+            printf("%-8.0f " % [asset[1] * (1 + asset[2] ) ** j ]  )
+        end
+        print "\n"
     end
+    print "\n"
 end
 
 assets = Array.new(1)
 assets[0] = 0
 assetCount = 0
 
-
-def newAsset(assets, assetCount)
-    newAsset = Array.new(3)
+def newAsset
+    tempAsset = Array.new(3)
     print "Enter asset name: "
-    newAsset[0] = gets.chomp
+    tempAsset[0] = gets.chomp
     
     print "Enter starting asset value: "
-    newAsset[1] = gets.to_f
+    tempAsset[1] = gets.to_f
     
     print "Enter asset growth rate (decimal): "
-    newAsset[2] = gets.to_f
+    tempAsset[2] = gets.to_f
 
-    assets[assetCount] = newAsset
-    assetCount = assetCount + 1
+    return tempAsset
 end
 
-def printAssets
+assets[assetCount] = newAsset
+assetCount = assetCount + 1
 
-    
-end
 
-newAsset(assets, assetCount)
+assets[assetCount] = newAsset
+assetCount = assetCount + 1
 
 printHeaders
-
-for asset in assets do
-    printf("%-8s" % [asset[0] ]  )
-    
-    for j in (0..YEARS) do
-        printf("%-8.0f " % [asset[1] * (1 + asset[2] ) ** j ]  )
-    end
-end
 printAssetData(assets)
-
-print("\n")
-
-print "assets length: "
-print assets.length
-print "\n"
-
-newAsset(assets, assetCount)
-
-print "assets length: "
-print assets.length
-print "\n"
